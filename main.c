60c9a5cd79ad09189573fde48236272b7afe0cd8
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-11 22:04:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,15 @@ int main(int ac, char **argv){
   char *lineptr;
   size_t n = 0; 
   ssize_t nchars_read;
+      printf("%s\n", "gamalwhile main ");
 
    /* declaring void variables */
   (void)ac; (void)argv;
 
     /* create an infinite loop */
     while (1){
+            printf("%s\n", "gamalwhile");
+
       printf("%s", prompt);
       nchars_read = getline(&lineptr, &n, stdin);
       /* check if the getline function failed or reached EOF or user use CTRL + D */ 
@@ -20,7 +23,7 @@ int main(int ac, char **argv){
         }
 
       printf("%s\n", lineptr);
-
+      printf("%s\n", "gamalwhile after if ");
       /* free up allocated memory */
       free(lineptr);    
     }
