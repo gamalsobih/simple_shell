930314852298f1204f8f83db35e66a62130c164e
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-11 20:24:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,11 @@
 
 int main(int ac, char **argv){
         char *promp = "(simple_shell) $ ";
-        printf("%s", promp);
         char *lineptr;
         size_t n = 0;
 	ssize_t nchars_read;
-        getline(&lineptr, &n, stdin);
         /* declaring void variables */
         (void)ac; (void)argv;
-
   	  while (1){
       printf("%s", promp);
       nchars_read = getline(&lineptr, &n, stdin);
