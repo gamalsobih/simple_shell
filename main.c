ff8f8db5675009e71e397ecd362d4f022edc5c09
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-11 22:19:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,26 @@
 #include "main.h"
 
 int main(int ac, char **argv){
-  char *prompt = "(Eshell) $ ";
+  char *prompt = "(simpleshell) $ ";
   char *lineptr;
   size_t n = 0; 
   ssize_t nchars_read;
-      printf("%s\n", "gamalwhile main ");
 
    /* declaring void variables */
   (void)ac; (void)argv;
-int x=0;
-    /* create an infinite loop */
-    while (x==0){
-            printf("%s\n", "gamalwhile");
 
+    /* create an infinite loop */
+    while (1){
       printf("%s", prompt);
       nchars_read = getline(&lineptr, &n, stdin);
       /* check if the getline function failed or reached EOF or user use CTRL + D */ 
         if (nchars_read == -1){
             printf("Exiting shell....\n");
-           printf("%s\n", "gamalwhile r if ");
             return (-1);
         }
 
       printf("%s\n", lineptr);
-      printf("%s\n", "gamalwhile after if ");
+
       /* free up allocated memory */
       free(lineptr);    
     }
