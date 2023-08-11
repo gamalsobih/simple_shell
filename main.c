258af39ba536387d0dc04ca2f845b7de7466ff9d
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-11 20:25:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 int main(int ac, char **argv){
-        char *promp = "(simple_shell) $ ";
-        char *lineptr;
-        size_t n = 0;
-	ssize_t nchars_read;
-        /* declaring void variables */
-        (void)ac; (void)argv;
-  	  while (1){
-      printf("%s", promp);
+  char *prompt = "(Eshell) $ ";
+  char *lineptr;
+  size_t n = 0; 
+  ssize_t nchars_read;
+
+   /* declaring void variables */
+  (void)ac; (void)argv;
+
+    /* create an infinite loop */
+    while (1){
+      printf("%s", prompt);
       nchars_read = getline(&lineptr, &n, stdin);
       /* check if the getline function failed or reached EOF or user use CTRL + D */ 
         if (nchars_read == -1){
@@ -22,5 +25,5 @@ int main(int ac, char **argv){
       free(lineptr);    
     }
 
-        return (0);
+  return (0);
 }
