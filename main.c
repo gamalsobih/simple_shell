e9068f7718b5884b4f5242a0fdaaa2afbb12917d
gamalsobih <gamalsob@gmail.com>
2023-08-11 10:23:05 -0700

    frst try of simple shell

--- main.c
+++ main.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
 int main(int ac, char **argv){
-	char *promp = "(simple_shell) $ ";
-	printf("%s", promp);
-	char *lineptr;
-	size_t n = 0; 
+        char *promp = "(simple_shell) $ ";
+        printf("%s", promp);
+        char *lineptr;
+        size_t n = 0;
+	ssize_t nchars_read;
+        getline(&lineptr, &n, stdin);
+        /* declaring void variables */
+        (void)ac; (void)argv;
 
-	getline(&lineptr, &n, stdin);
-	/* declaring void variables */
-	(void)ac; (void)argv;
+  	  while (1){
+      printf("%s", promp);
+      nchars_read = getline(&lineptr, &n, stdin);
+      /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
 
-	printf("%s\n", lineptr);
-	free(lineptr);
+      printf("%s\n", lineptr);
 
-	return (0);
+      /* free up allocated memory */
+      free(lineptr);    
+    }
+
+        return (0);
 }
-
