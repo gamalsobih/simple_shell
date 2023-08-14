e1997de89caa65fa667e482bdff676e545086972
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-15 02:46:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,38 @@
-#include "main.h"
+ #include "main.h"
 
 int main(int ac, char **argv){
-  char *prompt = "(simpleshell) $ ";
-  char *lineptr;
-  size_t n = 0; 
-  ssize_t nchars_read;
+    char *prompt = "(Eshell) $ ";
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    size_t n = 0;
+    ssize_t nchars_read;
+    const char *delim = " \n"
+    lineptr_copy = malloc(sizeof(char) * nchars_read);
+    if (lineptr_copy== NULL){
+        perror("tsh: memory allocation error");
+        return (-1);
+    }
 
-   /* declaring void variables */
-  (void)ac; (void)argv;
+    /* declaring void variables */
+    (void)ac; (void)argv;
 
-    /* create an infinite loop */
-    while (1){
-      printf("%s", prompt);
-      nchars_read = getline(&lineptr, &n, stdin);
-      /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+    /* Create a loop for the shell's prompt */
+    while (1) {
+        printf("%s", prompt);
+        nchars_read = getline(&lineptr, &n, stdin);
+        strcpy(lineptr_copy, lineptr);
+        /* check if the getline function failed or reached EOF or user use CTRL + D */ 
         if (nchars_read == -1){
             printf("Exiting shell....\n");
             return (-1);
         }
 
-      printf("%s\n", lineptr);
+        printf("%s\n", lineptr);
+        printf("%s%s\n", lineptr_copy ,"copy");
 
-      /* free up allocated memory */
-    }
-  free(lineptr);    
-  return (0);
+        /* free up allocated memory */ 
+        free(lineptr);
+    } 
+
+
+    return (0);
 }
