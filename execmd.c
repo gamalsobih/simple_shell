b25d73f9f637969582561255e9a9a41a9f9bb462
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-23 18:06:45 +0300

    Update execmd.c

--- execmd.c
+++ execmd.c
@@ -3,10 +3,9 @@
 void execmd(char **argv){
     char *command = NULL, *actual_command = NULL;
 
-    if (argv){
+    if (_strcmp(argv[0],"exit")!=0){
         /* get the command */
         command = argv[0];
-
         /* generate the path to this command before passing it to execve */
         actual_command = get_location(command);
 
@@ -14,6 +13,10 @@ void execmd(char **argv){
         if (execve(actual_command, argv, NULL) == -1){
             perror("Error:");
         }
+    }
+    if (_strcmp(argv[0],"exit")==0)
+    {
+        printf("%s","exit");
     }
 
 }
