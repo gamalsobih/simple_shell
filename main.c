51ae34ac7f49828714754fd6419203ca8e1b3b51
gamalsobih <gamalsob@gmail.com>
2023-08-24 05:15:18 -0700

    trying to solve some prlblems

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 int main(int ac, char **argv){
-    char *prompt = "(my-simple-shell) $ ";
+    char *prompt = "$";
     char *lineptr = NULL, *lineptr_copy = NULL;
     size_t n = 0;
     ssize_t nchars_read;
