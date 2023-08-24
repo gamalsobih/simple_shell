b1ded55945819cee3f8d27667884b996f4335480
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-24 17:13:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ int main(int ac, char **argv){
        nchars_read = getline(&lineptr, &n, stdin);
         /* check if the getline function failed or reached EOF or user use CTRL + D */ 
         if (nchars_read == -1){
-            printf("Exiting shell....\n");
             return (-1);
         }
 
