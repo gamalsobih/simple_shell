790b1a2c6dcbe057c0b575dc9f47aeae5016b516
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-25 05:28:46 +0300

    Update execmd.c

--- execmd.c
+++ execmd.c
@@ -15,6 +15,7 @@ void execmd(char **argv){
             envo();
         }
             else if(execve(actual_command, argv, NULL) == -1){
+                printf("gamal\n")
             perror("./shell");
         }
 
