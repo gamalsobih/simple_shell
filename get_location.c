e7109741275b40c5fb311cd945f263d2c9bcd46f
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-26 05:22:44 +0300

    Update get_location.c

--- get_location.c
+++ get_location.c
@@ -33,7 +33,8 @@ char *get_location(char *command){
 
                 /* free up allocated memory before returning your file_path */
                 free(path_copy);
-
+                free(path_token);
+                free(path);
                 return (file_path);
             }
             else{
@@ -50,13 +51,19 @@ char *get_location(char *command){
         /* before we exit without luck, let's see if the command itself is a file_path that exists */
         if (stat(command, &buffer) == 0)
         {
-            return (command);
+        free(path_token);
+        free(path);
+        free(path_copy);
+        free(file_path);
+        return (command);
         }
 
+        free(path);
 
         return (NULL);
 
     }
+        free(path);
 
 
     return (NULL);
