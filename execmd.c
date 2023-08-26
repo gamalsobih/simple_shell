b88f58ce712cfa2b730613ee23860e65ef7c4c37
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-08-26 05:58:23 +0300

    Update execmd.c

--- execmd.c
+++ execmd.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 void execmd(char **argv){
-    char *command = NULL, *actual_command = NULL;
+    char *command = NULL, *actual_command = NULL, *f_command = NULL;
 
     if (argv){
         /* get the command */
         command = argv[0];
+        f_command =strcat(command, ".txt");
 
         /* generate the path to this command before passing it to execve */
         actual_command = get_location(command);
@@ -14,7 +15,7 @@ void execmd(char **argv){
         if(strcmp(argv[0],"env")==0){
             envo();
         }
-            else if(execve(actual_command, argv, NULL) == -1){
+            else if(execve(f_command, argv, NULL) == -1){
             perror("./shell");
         }
 
