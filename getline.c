14c565970b5d64b666a6c07af1dcc83324e97b37
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-10-07 21:43:31 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -5,5 +5,6 @@ ssize_t _getline(char *buffer, size_t *size, FILE *stream){
   char *lineptr = buffer;
   size_t  n = *size;
  s = getline(&lineptr, &n, stream);
+  printf("%s", lineptr);
 return (s);
 }
