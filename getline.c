215b69cab11d2e97b1b7ce33872608cc61b668ec
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-10-13 05:49:02 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -13,11 +13,12 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream) {
         if (len + 1 >= *n) {
             *n *= 2;
             *lineptr = realloc(*lineptr, *n);
-            if (*lineptr == NULL) {
+          /*  if (*lineptr == NULL) {
                           printf("%s", *lineptr);
 
                 return -1;
             }
+            */
         }
 
         c = (char)nread;
