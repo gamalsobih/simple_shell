5b02a3cf8760d2f97e76e04a869d23655c32fd17
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-10-14 01:52:31 +0300

    Update mystrtok.c

--- mystrtok.c
+++ mystrtok.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 char *_strtok(char *str, const char *delim) {
-  // Declare a static variable to store the current position in the string
+  /* Declare a static variable to store the current position in the string*/
   static char *pos;
-  // Declare a variable to store the pointer to the token
+  /* Declare a variable to store the pointer to the token*/
   char *token;
-  // If str is not NULL, set pos to str
+  /* If str is not NULL, set pos to str*/
   if (str != NULL) {
     pos = str;
   }
-  // If pos is NULL, return NULL
+  /* If pos is NULL, return NULL*/
   if (pos == NULL) {
     return NULL;
   }
-  // Skip any leading delimiters by using strspn
+  /*Skip any leading delimiters by using strspn*/
   pos += strspn(pos, delim);
-  // If pos points to a null character, return NULL
+  /* If pos points to a null character, return NULL*/
   if (*pos == '\0') {
     return NULL;
   }
-  // Set token to pos
+  /* Set token to pos*/
   token = pos;
-  // Find the next delimiter by using strpbrk
+  /* Find the next delimiter by using strpbrk*/
   pos = strpbrk(token, delim);
-  // If pos is not NULL, replace the delimiter with a null character and increment pos
+  /* If pos is not NULL, replace the delimiter with a null character and increment pos*/
   if (pos != NULL) {
     *pos = '\0';
     pos++;
