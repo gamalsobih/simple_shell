44ebeb5225561a2c5d97bc7f731261bf4b6c5fbb
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-10-13 06:18:44 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -8,6 +8,37 @@
  * @j: size of buffer
  */
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		return (NULL);
+
+	if (new_size < old_size)
+		_memcpy(newptr, ptr, new_size);
+	else
+		_memcpy(newptr, ptr, old_size);
+
+	free(ptr);
+	return (newptr);
+}
+
+
+
 char *_strcpy(char *dest, char *src)
 {
 
