0ab930ce85f5bb5fc90982a9d169864eef265f3a
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-10-13 06:14:13 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,38 +1,84 @@
 #include "main.h"
 
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream) {
-    size_t len = 0;
-    ssize_t nread;
-    char c;
+/**
+ * bring_line - assigns the line var for get_line
+ * @lineptr: Buffer that store the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
+ */
+void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
+{
 
-    if (lineptr == NULL || n == NULL || stream == NULL) {
-        return -1;
-    }
+	if (*lineptr == NULL)
+	{
+		if  (j > BUFSIZE)
+			*n = j;
 
-    while ((nread = fgetc(stream)) != EOF) {
-      /*   if (len + 1 >= *n) {
-            *n *= 2;
-            *lineptr = realloc(*lineptr, *n);
-           if (*lineptr == NULL) {
-                          printf("%s", *lineptr);
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else if (*n < j)
+	{
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+/**
+ * get_line - Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
+ */
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
 
-                return -1;
-            }
-            
-        }*/
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
 
-        c = (char)nread;
-        (*lineptr)[len++] = c;
-
-        if (c == '\n') {
-            break;
-        }
-    }
-
-    if (len == 0) {
-        return -1;
-    }
-
-    (*lineptr)[len] = '\0';
-    return len;
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == 0)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
+	}
+	buffer[input] = '\0';
+	bring_line(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
 }
